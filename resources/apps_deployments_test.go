577d5dd0482347daf1ce4504cb50fb4aa7a27db4
Andrii Romanenko <andrejjrom@gmail.com>
2021-10-28 13:32:10 +0300

    merge main

--- resources/apps_deployments_test.go
+++ resources/apps_deployments_test.go
@@ -3,58 +3,39 @@ package resources
 import (
 	"github.com/cloudquery/cq-provider-k8s/client"
 	"github.com/cloudquery/cq-provider-k8s/client/mocks"
-	"github.com/cloudquery/faker/v3"
 	"github.com/golang/mock/gomock"
 	appsv1 "k8s.io/api/apps/v1"
-	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"testing"
 )
 
 func createAppsDeployments(t *testing.T, ctrl *gomock.Controller) client.Services {
-	nodes := mocks.NewMockNodesClient(ctrl)
-	nodes.EXPECT().List(gomock.Any(), metav1.ListOptions{}).Return(
-		&corev1.NodeList{Items: []corev1.Node{fakeNode(t)}}, nil,
+	deploymentsClient := mocks.NewMockDeploymentsClient(ctrl)
+	deploymentsClient.EXPECT().List(gomock.Any(), metav1.ListOptions{}).Return(
+		&appsv1.DeploymentList{Items: []appsv1.Deployment{fakeAppsDeployment(t)}}, nil,
 	)
 	return client.Services{
-		Nodes: nodes,
+		Deployments: deploymentsClient,
 	}
 }
 
-func fakeAppsDeployments(t *testing.T) appsv1.Deployment {
-	// faker chokes on Node.Status.{Capacity,Allocatable} so doing it by hand
+func fakeAppsDeployment(t *testing.T) appsv1.Deployment {
 	var deployment appsv1.Deployment
-	ptrs := []interface{}{
+	fakeThroughPointers(t, []interface{}{
 		&deployment.TypeMeta,
 		&deployment.ObjectMeta,
-		&deployment.Spec,
 		&deployment.Status,
-	}
-	for i, ptr := range ptrs {
-		if err := faker.FakeData(ptr); err != nil {
-			t.Fatalf("%v %v", i, ptr)
-		}
-	}
-	//rl := make(corev1.ResourceList)
-	//rl["name"] = *apiresource.NewQuantity(1024*1024, apiresource.BinarySI)
-	//deployment.Status.Capacity = rl
-	//node.Status.Allocatable = rl
-	//node.Spec.PodCIDR = "192.168.1.0/24"
-	//node.Spec.PodCIDRs = []string{"192.168.1.0/24"}
-	//node.Status.Addresses = []corev1.NodeAddress{
-	//	{
-	//		Type:    corev1.NodeHostName,
-	//		Address: "testname",
-	//	},
-	//	{
-	//		Type:    corev1.NodeInternalIP,
-	//		Address: "fd00::1",
-	//	},
-	//	{
-	//		Type:    corev1.NodeExternalIP,
-	//		Address: "192.168.2.1",
-	//	},
-	//}
+		&deployment.ManagedFields,
+		&deployment.Annotations,
+		&deployment.Labels,
+		&deployment.OwnerReferences,
+		&deployment.Status,
+		&deployment.Spec.Selector,
+		&deployment.Spec.RevisionHistoryLimit,
+	})
+
+	deployment.Spec.Template = fakePodTemplateSpec(t)
+
 	return deployment
 }
 
