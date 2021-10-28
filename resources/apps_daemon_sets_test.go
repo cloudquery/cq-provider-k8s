c429d6d7bc69faf65ff2da6e799a1fec155290b0
Andrii Romanenko <andrejjrom@gmail.com>
2021-10-28 13:27:17 +0300

    tests simplification

--- resources/apps_daemon_sets_test.go
+++ resources/apps_daemon_sets_test.go
@@ -1,13 +1,13 @@
 package resources
 
 import (
+	"testing"
+
 	"github.com/cloudquery/cq-provider-k8s/client"
 	"github.com/cloudquery/cq-provider-k8s/client/mocks"
 	"github.com/golang/mock/gomock"
 	appsv1 "k8s.io/api/apps/v1"
-	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	"testing"
 )
 
 func createAppsDaemonSets(t *testing.T, ctrl *gomock.Controller) client.Services {
@@ -18,48 +18,6 @@ func createAppsDaemonSets(t *testing.T, ctrl *gomock.Controller) client.Services
 	return client.Services{
 		DaemonSets: daemonSetsClient,
 	}
-}
-
-func fakeDaemonSet(t *testing.T) appsv1.DaemonSet {
-	var ds appsv1.DaemonSet
-	ds.Spec.Template.Spec.Volumes = []corev1.Volume{fakeVolume(t)}
-	fakeThroughPointers(t, []interface{}{
-		&ds.TypeMeta,
-		&ds.ObjectMeta,
-		&ds.Status,
-		&ds.ManagedFields,
-		&ds.Annotations,
-		&ds.Labels,
-		&ds.OwnerReferences,
-		&ds.Status,
-		&ds.Spec.Selector,
-		&ds.Spec.RevisionHistoryLimit,
-	})
-
-	ds.Spec.Template = fakePodTemplateSpec(t)
-	return ds
-}
-
-func fakePodTemplateSpec(t *testing.T) corev1.PodTemplateSpec {
-	var templateSpec corev1.PodTemplateSpec
-	fakeThroughPointers(t, []interface{}{
-		&templateSpec.Annotations,
-		&templateSpec.Name,
-		&templateSpec.GenerateName,
-		&templateSpec.Namespace,
-		&templateSpec.SelfLink,
-		&templateSpec.UID,
-		&templateSpec.ResourceVersion,
-		&templateSpec.Generation,
-		&templateSpec.DeletionGracePeriodSeconds,
-		&templateSpec.Labels,
-		&templateSpec.Finalizers,
-		&templateSpec.ClusterName,
-		&templateSpec.OwnerReferences,
-		&templateSpec.ManagedFields,
-	})
-	templateSpec.Spec = fakePodSpec(t)
-	return templateSpec
 }
 
 func TestAppsDaemonSets(t *testing.T) {
