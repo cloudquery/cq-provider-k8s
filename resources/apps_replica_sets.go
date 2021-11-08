b333699fb9531b80c414480885be8267f78b16a1
Andrii Romanenko <andrejjrom@gmail.com>
2021-11-08 10:57:43 +0200

    meta data is in json columns now

--- resources/apps_replica_sets.go
+++ resources/apps_replica_sets.go
@@ -72,6 +72,11 @@ func AppsReplicaSets() *schema.Table {
 				Resolver: schema.PathResolver("ObjectMeta.Annotations"),
 			},
 			{
+				Name:     "owner_references",
+				Type:     schema.TypeJSON,
+				Resolver: resolveAppsReplicaSetOwnerReferences,
+			},
+			{
 				Name:     "finalizers",
 				Type:     schema.TypeStringArray,
 				Resolver: schema.PathResolver("ObjectMeta.Finalizers"),
@@ -80,6 +85,11 @@ func AppsReplicaSets() *schema.Table {
 				Name:     "cluster_name",
 				Type:     schema.TypeString,
 				Resolver: schema.PathResolver("ObjectMeta.ClusterName"),
+			},
+			{
+				Name:     "managed_fields",
+				Type:     schema.TypeJSON,
+				Resolver: resolveAppsReplicaSetManagedFields,
 			},
 			{
 				Name:        "replicas",
@@ -330,90 +340,6 @@ func AppsReplicaSets() *schema.Table {
 			},
 		},
 		Relations: []*schema.Table{
-			{
-				Name:        "k8s_apps_replica_set_owner_references",
-				Description: "OwnerReference contains enough information to let you identify an owning object",
-				Resolver:    fetchAppsReplicaSetOwnerReferences,
-				Columns: []schema.Column{
-					{
-						Name:        "replica_set_cq_id",
-						Description: "Unique CloudQuery ID of k8s_apps_replica_sets table (FK)",
-						Type:        schema.TypeUUID,
-						Resolver:    schema.ParentIdResolver,
-					},
-					{
-						Name:        "api_version",
-						Description: "API version of the referent.",
-						Type:        schema.TypeString,
-						Resolver:    schema.PathResolver("APIVersion"),
-					},
-					{
-						Name:        "kind",
-						Description: "Kind of the referent. More info: https://git.k8s.io/community/contributors/devel/sig-architecture/api-conventions.md#types-kinds",
-						Type:        schema.TypeString,
-					},
-					{
-						Name:        "name",
-						Description: "Name of the referent. More info: http://kubernetes.io/docs/user-guide/identifiers#names",
-						Type:        schema.TypeString,
-					},
-					{
-						Name:        "uid",
-						Description: "UID of the referent. More info: http://kubernetes.io/docs/user-guide/identifiers#uids",
-						Type:        schema.TypeString,
-						Resolver:    schema.PathResolver("UID"),
-					},
-					{
-						Name:        "controller",
-						Description: "If true, this reference points to the managing controller. +optional",
-						Type:        schema.TypeBool,
-					},
-					{
-						Name:        "block_owner_deletion",
-						Description: "If true, AND if the owner has the \"foregroundDeletion\" finalizer, then the owner cannot be deleted from the key-value store until this reference is removed. Defaults to false. To set this field, a user needs \"delete\" permission of the owner, otherwise 422 (Unprocessable Entity) will be returned. +optional",
-						Type:        schema.TypeBool,
-					},
-				},
-			},
-			{
-				Name:        "k8s_apps_replica_set_managed_fields",
-				Description: "ManagedFieldsEntry is a workflow-id, a FieldSet and the group version of the resource that the fieldset applies to.",
-				Resolver:    fetchAppsReplicaSetManagedFields,
-				Columns: []schema.Column{
-					{
-						Name:        "replica_set_cq_id",
-						Description: "Unique CloudQuery ID of k8s_apps_replica_sets table (FK)",
-						Type:        schema.TypeUUID,
-						Resolver:    schema.ParentIdResolver,
-					},
-					{
-						Name:        "manager",
-						Description: "Manager is an identifier of the workflow managing these fields.",
-						Type:        schema.TypeString,
-					},
-					{
-						Name:        "operation",
-						Description: "Operation is the type of operation which lead to this ManagedFieldsEntry being created. The only valid values for this field are 'Apply' and 'Update'.",
-						Type:        schema.TypeString,
-					},
-					{
-						Name:        "api_version",
-						Description: "APIVersion defines the version of this resource that this field set applies to",
-						Type:        schema.TypeString,
-						Resolver:    schema.PathResolver("APIVersion"),
-					},
-					{
-						Name:        "fields_type",
-						Description: "FieldsType is the discriminator for the different fields format and version. There is currently only one possible value: \"FieldsV1\"",
-						Type:        schema.TypeString,
-					},
-					{
-						Name:        "subresource",
-						Description: "Subresource is the name of the subresource used to update that object, or empty string if the object was updated through the main resource",
-						Type:        schema.TypeString,
-					},
-				},
-			},
 			{
 				Name:        "k8s_apps_replica_set_selector_match_expressions",
 				Description: "A label selector requirement is a selector that contains values, a key, and an operator that relates the key and values.",
@@ -1637,6 +1563,28 @@ func fetchAppsReplicaSets(ctx context.Context, meta schema.ClientMeta, parent *s
 		opts.Continue = result.GetContinue()
 	}
 }
+func resolveAppsReplicaSetOwnerReferences(ctx context.Context, meta schema.ClientMeta, resource *schema.Resource, c schema.Column) error {
+	p, ok := resource.Item.(appsv1.ReplicaSet)
+	if !ok {
+		return fmt.Errorf("not a appsv1.ReplicaSet instance: %T", resource.Item)
+	}
+	b, err := json.Marshal(p.OwnerReferences)
+	if err != nil {
+		return err
+	}
+	return resource.Set(c.Name, b)
+}
+func resolveAppsReplicaSetManagedFields(ctx context.Context, meta schema.ClientMeta, resource *schema.Resource, c schema.Column) error {
+	p, ok := resource.Item.(appsv1.ReplicaSet)
+	if !ok {
+		return fmt.Errorf("not a appsv1.ReplicaSet instance: %T", resource.Item)
+	}
+	b, err := json.Marshal(p.ManagedFields)
+	if err != nil {
+		return err
+	}
+	return resource.Set(c.Name, b)
+}
 func resolveAppsReplicaSetTemplateSpecSecurityContext(ctx context.Context, meta schema.ClientMeta, resource *schema.Resource, c schema.Column) error {
 	p, ok := resource.Item.(appsv1.ReplicaSet)
 	if !ok {
@@ -1711,22 +1659,6 @@ func resolveAppsReplicaSetTemplateSpecTopologySpreadConstraints(ctx context.Cont
 		return err
 	}
 	return resource.Set(c.Name, b)
-}
-func fetchAppsReplicaSetOwnerReferences(ctx context.Context, meta schema.ClientMeta, parent *schema.Resource, res chan interface{}) error {
-	p, ok := parent.Item.(appsv1.ReplicaSet)
-	if !ok {
-		return fmt.Errorf("not a appsv1.ReplicaSet instance: %T", parent.Item)
-	}
-	res <- p.OwnerReferences
-	return nil
-}
-func fetchAppsReplicaSetManagedFields(ctx context.Context, meta schema.ClientMeta, parent *schema.Resource, res chan interface{}) error {
-	p, ok := parent.Item.(appsv1.ReplicaSet)
-	if !ok {
-		return fmt.Errorf("not a appsv1.ReplicaSet instance: %T", parent.Item)
-	}
-	res <- p.ManagedFields
-	return nil
 }
 func fetchAppsReplicaSetSelectorMatchExpressions(ctx context.Context, meta schema.ClientMeta, parent *schema.Resource, res chan interface{}) error {
 	p, ok := parent.Item.(appsv1.ReplicaSet)
