45e260c215041a2f151409fb3ca139088f0ab173
bbernays <ben@bernays.me>
2021-12-29 13:08:22 -0500

    Merge

--- resources/provider/provider.go
+++ resources/provider/provider.go
@@ -3,6 +3,10 @@ package provider
 import (
 	"github.com/cloudquery/cq-provider-k8s/client"
 	"github.com/cloudquery/cq-provider-k8s/resources/services/apps"
+	"github.com/cloudquery/cq-provider-k8s/resources/services/batch"
+	"github.com/cloudquery/cq-provider-k8s/resources/services/core"
+	"github.com/cloudquery/cq-provider-k8s/resources/services/networking"
+	"github.com/cloudquery/cq-provider-k8s/resources/services/rbac"
 
 	"github.com/cloudquery/cq-provider-sdk/provider"
 	"github.com/cloudquery/cq-provider-sdk/provider/schema"
@@ -23,23 +27,23 @@ func Provider() *provider.Provider {
 			return &client.Config{}
 		},
 		ResourceMap: map[string]*schema.Table{
-			"apps.daemon_sets":   apps.DaemonSets(),
-			"apps.deployments":   apps.Deployments(),
-			"apps.replica_sets":  apps.ReplicaSets(),
-			"apps.stateful_sets": apps.StatefulSets(),
-			// "batch.cron_jobs":             batch.CronJobs(),
-			// "batch.jobs":                  batch.Jobs(),
-			// "core.endpoints":              core.Endpoints(),
-			// "core.limit_ranges":           core.LimitRanges(),
-			// "core.namespaces":             core.Namespaces(),
-			// "core.nodes":                  core.Nodes(),
-			// "core.pods":                   core.Pods(),
-			// "core.resource_quotas":        core.ResourceQuotas(),
-			// "core.service_accounts":       core.ServiceAccounts(),
-			// "core.services":               core.Services(),
-			// "networking.network_policies": networking.NetworkPolicies(),
-			// "rbac.role_bindings":          rbac.RoleBindings(),
-			// "rbac.roles":                  rbac.Roles(),
+			"apps.daemon_sets":            apps.DaemonSets(),
+			"apps.deployments":            apps.Deployments(),
+			"apps.replica_sets":           apps.ReplicaSets(),
+			"apps.stateful_sets":          apps.StatefulSets(),
+			"batch.cron_jobs":             batch.CronJobs(),
+			"batch.jobs":                  batch.Jobs(),
+			"core.endpoints":              core.Endpoints(),
+			"core.limit_ranges":           core.LimitRanges(),
+			"core.namespaces":             core.Namespaces(),
+			"core.nodes":                  core.Nodes(),
+			"core.pods":                   core.Pods(),
+			"core.resource_quotas":        core.ResourceQuotas(),
+			"core.service_accounts":       core.ServiceAccounts(),
+			"core.services":               core.Services(),
+			"networking.network_policies": networking.NetworkPolicies(),
+			"rbac.role_bindings":          rbac.RoleBindings(),
+			"rbac.roles":                  rbac.Roles(),
 		},
 	}
 }
